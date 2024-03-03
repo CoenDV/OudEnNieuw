362a63556c0420d76aedaad5b68953673dc5ffa5
CoenDV <c.oen@hotmail.nl>
2024-03-03 10:48:10 +0100

    routing starter

---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <RouterLink to="/" class="nav-link" active-class="active">Go to Home</RouterLink> 
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/CreateProduct" class="nav-link" active-class="active">Create Product</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/ProductList" class="nav-link" active-class="active">Products</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../components/Home.vue';
+import CreateProduct from '../components/CreateProduct.vue';
+import ProductList from '../components/ProductList.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/CreateProduct', component: CreateProduct },
+    { path: '/ProductList', component: ProductList }
+  ]
+})
+
+export default router
