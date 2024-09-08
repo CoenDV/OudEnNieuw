2167b7d886e67f4c1d81ad65383ddec676bd4f4b
CoenDV <c.oen@hotmail.nl>
2024-09-08 12:46:20 +0200

    change host

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-// baseURL: 'https://oudennieuwbackend.onrender.com/'
-baseURL: 'http://localhost:8080/'
+baseURL: 'https://oudennieuwbackend.onrender.com/'
+// baseURL: 'http://localhost:8080/'
 });
 export default instance;
--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-// app.config.globalProperties.$webSocketLink = 'wss://oudennieuwbackend.onrender.com/'
-app.config.globalProperties.$webSocketLink = 'ws://localhost:8080/'
+app.config.globalProperties.$webSocketLink = 'wss://oudennieuwbackend.onrender.com/'
+// app.config.globalProperties.$webSocketLink = 'ws://localhost:8080/'
 
 app.use(createPinia())
 app.use(router)
