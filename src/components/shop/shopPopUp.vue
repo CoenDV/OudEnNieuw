239baf873bba01a3262fd2a96f9b5a1bbef32b63
CoenDV <c.oen@hotmail.nl>
2024-12-07 21:08:33 +0100

    update shop component to manage audio playback and add video intro; adjust deadline logic and styles

--- src/components/shop/shopPopUp.vue
+++ src/components/shop/shopPopUp.vue
@@ -1,6 +1,8 @@
 <script>
 import Header from './../Header.vue';
 
+// https://www.myinstants.com/en/categories/sound%20effects/
+
 export default {
     name: "Shop",
     components: {
@@ -8,14 +10,25 @@ export default {
     },
     data() {
         return {
-
+            sounds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            currentSound: null
         }
     },
     methods: {
-
+        // get random sound
+        getRandomSound() {
+            return this.sounds[Math.floor(Math.random() * this.sounds.length)];
+        },
     },
     mounted() {
-        document.getElementById('myAudio').play();
+        this.currentSound = this.getRandomSound();
+        //document.getElementById('myAudio').play();
+
+        document.getElementById('intro').addEventListener('ended', myHandler, false);
+        function myHandler(e) {
+            // hide the intro video
+            document.querySelector('#intro').style.display = 'none';
+        }
     },
     props: {
         item: {
@@ -27,23 +40,24 @@ export default {
 </script>
 
 <template>
-    <img id="confetti" src="/images/confetti.gif" alt="confetti">
-    <div class="modal show">
+    
+    <div class="modal z-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body row justify-content-center">
                     <img :src="'./images/shop/' + item.title + '.png'" alt="shop item" class="img-fluid">
-                    <h1 class="modal-title text-center">{{ item.title }}</h1>
-                    <p class="text-center">{{ item.explanation }}</p>
-                    <p class="text-center">Points: {{ item.points }}</p>
                 </div>
             </div>
         </div>
     </div>
-    <audio id="myAudio">
-        <source src="/audio/purchaseItem.mp3" type="audio/mpeg">
+
+    <video id="intro" style="display: block;" class="" width="100%" height="100%" autoplay>
+        <source class="z-3" src="./../../../public/audio/theHub.mp4" type="video/mp4">
+    </video>
+    <!-- <audio id="myAudio">
+        <source src="/audio/thehub.mp3" type="audio/mpeg">
         Your browser does not support the audio element.
-    </audio>
+    </audio> -->
 </template>
 
 <style scoped>
@@ -55,7 +69,6 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
-    z-index: 1050;
     overflow: auto;
 }
 
@@ -66,7 +79,7 @@ export default {
     /* https://animate.style */
     animation: jackInTheBox, lightSpeedOutRight;
     animation-duration: 2s, 1s;
-    animation-delay: 0s, 19s;
+    animation-delay: 3s, 19s;
 }
 
 .modal-header {
@@ -85,6 +98,10 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
-    z-index: 1049;
+}
+
+#intro {
+    position: relative; /* Ensure it participates in stacking context */
+    z-index: 10000; /* Higher than modal's default z-index of 1050 */
 }
 </style>
